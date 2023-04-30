69bd13a6975305f0ddbfd4b210bfac22791ce616
SuzuhaAmane27 <24alexandro24@mail.ru>
2023-04-30 20:25:27 +0300

    remove comm

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -74,50 +74,5 @@ public class AdminController {
 
 }
 
-//    @GetMapping()
-//    public String hello(Model model) {
-//        model.addAttribute("users", userService.getAllUsers());
-//
-//
-//        return "usersList";
-//    }
-//    @GetMapping("/{id}")
-//    public String show(@PathVariable("id")int id, Model model) {
-//        model.addAttribute("user", userService.getUser(id));
-//        return "userPage";
-//    }
-//
-//    @GetMapping("/new")
-//    private String createUser(Model model) {
-//        model.addAttribute("user", new User());
-//
-//        return "new";
-//    }
-//
-//    @PostMapping()
-//    public String create(@ModelAttribute("user")User user) {
-//        userService.saveUser(user); //добавить метод
-//        return "redirect:/list";
-//    }
-//
-//    @GetMapping("/{id}/edit")
-//    public String edit(Model model, @PathVariable("id") int id) {
-//        model.addAttribute("user", userService.getUser(id));
-//        return "edit";
-//
-//    }
-//
-//    @PatchMapping("/{id}")
-//    public String update(@ModelAttribute("user")User user, @PathVariable("id") int id) {
-//        userService.update(id, user);
-//        return "redirect:/list";
-//    }
-//    @Role(2)
-//    @DeleteMapping("/{id}")
-//    public String delete(@PathVariable("id")int id) {
-//        userService.delete(id);
-//        return "redirect:/list";
-//    }
-//
-//}
+
 
